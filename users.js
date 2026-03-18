7e97d9c89b009e59c4d82d655106289dbe2fe4b8
carcenter-p <udi@carcenter.co.il>
2026-03-18 07:57:55 +0200

    Update users.js

--- users.js
+++ users.js
@@ -1,9 +1,7 @@
-
-
 const users = [
-  { id:1, username:'ישראל', password:'1234',     display:'ישראל כ.', role:'worker' },
-  { id:2, username:'משה',   password:'1234',     display:'משה ל.',   role:'worker' },
-  { id:3, username:'דוד',   password:'1234',     display:'דוד מ.',   role:'worker' },
-  { id:4, username:'admin', password:'admin123', display:'מנהל',     role:'admin'  }
+  { id:1, username:'אודי',  password:'1234',     display:'אודי',    role:'worker' },
+  { id:2, username:'משה',   password:'1234',     display:'משה ל.',  role:'worker' },
+  { id:3, username:'דוד',   password:'1234',     display:'דוד מ.',  role:'worker' },
+  { id:4, username:'admin', password:'admin123', display:'מנהל',    role:'admin'  }
 ];
 module.exports = users;
